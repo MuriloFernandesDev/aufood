dd67704e656e7b6ba5b134b94699c6be6bd0e834
Murilo Fernandes <muriloflimaa@gmail.com>
2023-06-12 20:20:13 -0300

    Add files via upload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
+import axios from 'axios'
 import dynamic from 'next/dynamic'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { HeaderComponentHome } from '../components/Home/Header'
 import SearchHome from '../components/Home/Search'
 const CategoriesComponent = dynamic(
@@ -8,6 +9,26 @@ const CategoriesComponent = dynamic(
 )
 
 const Home = () => {
+   const getDados = async () => {
+     await axios("https://app.excelent.com.br/api/request/Lancamentos/Pesquisar", {
+      headers: {
+         Accept: "application/json",
+         "Authorization-Token":"8f61667acffeb2c74706360afb1d0f7d0a0037e22b772d9504a5f24b882b5b4538c0c80ea742387caec355e17cd4c8bffafb7a614be84771eb7bdd66c5f204d488fe1ede37c1a3f816f323edbd4c138cc52724dfc77d745e88abd37755e22d838f441f1233fdad10b2a21e7f92892d8e2c62c1a2948b733da03dfe6ae7fedd81",
+         User: "gestao@excelent.com.br",
+         App: "API"
+      },
+    }).then((res) => {
+      console.log(res)
+    }).catch((error) => {
+      console.log(error)
+    })
+
+   }
+
+   useEffect(() => {
+      getDados()
+   }, [])
+
    return (
       <React.Fragment>
          <HeaderComponentHome />
