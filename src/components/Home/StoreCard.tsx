c08ad5a38fcb7913f56a82e7f6998b5eeaf42d05
Murilo Fernandes <muriloflimaa@gmail.com>
2023-08-17 22:08:25 -0300

    Ajustando card loja home

--- src/components/Home/StoreCard.tsx
+++ src/components/Home/StoreCard.tsx
@@ -4,10 +4,11 @@ import { FaStar } from 'react-icons/fa'
 const StoreCart = () => {
    return (
       <Link href="/McDonalds">
-         <div className="card flex flex-col md:flex-row md:hover:scale-105 cursor-pointer md:hover:shadow-xl transition-all duration-300 items-center bg-base-100 gap-2 col-span-2 md:col-span-1 mt-4">
+         <div className="card shadow-md md:shadow-none flex flex-col md:flex-row md:hover:scale-105 cursor-pointer md:hover:shadow-xl transition-all duration-300 items-center bg-base-100 gap-2 col-span-2 md:col-span-1 mt-4">
             <img
                width={100}
                height={100}
+               className="rounded-md md:rounded-none"
                src="https://www.macarthursquare.com.au/globalassets/retail/macarthur/stores/retailer-logos/mcdonalds-logo.jpg?width=300&height=400&upscale=false&mode=max&quality=80"
             />
             <div className="card-body p-0 flex flex-col gap-1 text-xs">
