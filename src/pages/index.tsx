e119f468632bbe67e9d2cb0a6f4519533db703d3
Murilo Fernandes <muriloflimaa@gmail.com>
2024-01-11 15:16:55 -0300

    ajustes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,7 +16,7 @@ const Home = () => {
    const [listAllStores, setListAllStores] = useState<IStoreListAll[]>([])
 
    useEffect(() => {
-      api.get('/store/list_all').then((response) => {
+      api.get('/store/list_all_store').then((response) => {
          setListAllStores(response.data)
       })
    }, [])
